be5fd0981b48ae4959a3aff30a2daaa244dfa7d8
Thiago Canete <thiago.canete@gmail.com>
2020-03-08 17:09:35 -0300

    Adição da documentação da API

--- API/ControlePagamentoWebApi/ControlePagamentoWebApi/Controllers/HealthChecksController.cs
+++ API/ControlePagamentoWebApi/ControlePagamentoWebApi/Controllers/HealthChecksController.cs
@@ -5,9 +5,17 @@ namespace ControlePagamentoWebApi.Controllers
 {
     public class HealthChecksController : Controller
     {
-        public const string RoutePath = "/healthz";
+        public const string RoutePath = "api/v1/healthz";
 
+        /// <summary>
+        /// Retorna o status da aplicação
+        /// </summary>asd asdasd
+        /// <returns>O status de funcionamento da API</returns>
+        /// <response code="200">Reotrno caso a API esteja OK</response>
+        /// <response code="500">Reotrno caso a API esteja com problemas</response>
         [HttpGet(RoutePath)]
+        [ProducesResponseType(typeof(void), 200)]
+        [ProducesResponseType(typeof(void), 500)]
         public IActionResult Healthz()
         {
             return Ok(DateTimeOffset.Now);
--- API/ControlePagamentoWebApi/ControlePagamentoWebApi/Controllers/PagamentosController.cs
+++ API/ControlePagamentoWebApi/ControlePagamentoWebApi/Controllers/PagamentosController.cs
@@ -1,15 +1,26 @@
 ﻿using ControlePagamentoWebApi.Dto;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace ControlePagamentoWebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PagamentosController : ControllerBase
     {
+        /// <summary>
+        /// Obtem informações relacionadas a um pagamento especifico
+        /// </summary>
+        /// <param name="id">ID do pagamento</param>
+        /// <returns>Pagamento solicitado</returns>
+        /// <response code="200">Caso o pagamento seja bem sucedido</response>
+        /// <response code="400">Caso o pagamento não seja encontrado</response>
+        /// <response code="500">Caso ocorra um erro desconhecido</response>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(PagamentoDto), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public IActionResult ObtemPagamento(Guid id)
         {
             PagamentoDto pagamentoDto = new PagamentoDto
@@ -24,8 +35,18 @@ namespace ControlePagamentoWebApi.Controllers
             return Ok(pagamentoDto);
         }
 
+        /// <summary>
+        /// Realiza o cadastro de um pagamento
+        /// </summary>
+        /// <param name="pagamentoPost">Estrutura de pagamento</param>
+        /// <returns>Pagamento realizado</returns>
+        /// <response code="200">Caso o pagamento seja bem sucedido</response>
+        /// <response code="400">Caso ocorra um erro conhecido no processamento</response>
+        /// <response code="500">Caso ocorra um erro desconhecido</response>
         [HttpPost]
         [ProducesResponseType(typeof(PagamentoDto), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public IActionResult RealizaPagamento(PagamentoPost pagamentoPost)
         {
             PagamentoDto pagamentoDto = new PagamentoDto
