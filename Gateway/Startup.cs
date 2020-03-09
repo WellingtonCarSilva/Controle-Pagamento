ecbdd9ff3f69bbbed9527f4e63e173ae473545e4
Vinicius Oliveira <oliveira.vinicius95@gmail.com>
2020-03-09 20:34:59 -0300

    Tentativa de configurar Gateway

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Gateway
 {
